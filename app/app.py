47eeefe04bcbc5ec45b5942a3846eea32d837c5d
RayyanMinhaj <81464495+RayyanMinhaj@users.noreply.github.com>
2024-08-16 20:48:50 +0500

    testing

--- app/app.py
+++ app/app.py
@@ -265,7 +265,7 @@ def download_file():
 
 @app.route('/hello', methods=['GET'])
 def hello_world():
-    return 'Hello, World!'
+    return key
 
 if __name__ == '__main__':
     app.run(debug=True)
